c007f5da836034bf5bcc6b9a1fe1ffe2fa034fdb
nblxck <nhsn.hasna@gmail.com>
2022-04-25 23:17:13 +0700

    Add example

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,111 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useState, useEffect } from 'react'
+import {ethers} from 'ethers';
+// import './App.css'
+
+
+
+const ADDRESS = "0xCDecf16fDCD0FC7b3D3bf2010b8EF72052747d10";
+const ABI = [
+	{
+		"inputs": [],
+		"name": "donate",
+		"outputs": [],
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "_message",
+				"type": "string"
+			}
+		],
+		"name": "setMessage",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "getBalance",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "getMessage",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+]
+
+let rieContract;
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [text, setText] = useState('');
+  const [message, setMessage] = useState('');
+
+  useEffect( () => {
+    connect();
+  }, [] );
+  
+  async function connect() {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    await provider.send("eth_requestAccounts", []);
+    const signer = provider.getSigner();
+    rieContract = new ethers.Contract(ADDRESS, ABI, signer);
+    console.log("Hello: ", rieContract);
+  }
+
+  async function getMessage() {
+    const mesg = await rieContract.getMessage()
+    setMessage(mesg);
+    console.log("Get message: " + mesg)
+  }
+
+  async function setUIMessage() {
+    const tx = await rieContract.setMessage(text);
+    await tx.wait();
+    await getMessage();
+  }
+
+  async function donate() {
+    const tx = await rieContract.donate({value: ethers.utils.parseEther("0.01")});
+    await tx.wait();
+    console.log("Donate successful");
+  }
+
+  async function getBalance() {
+    
+    console.log();
+  }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+   <div>
+     <center>
+      <h1>Hello:  {message}</h1>
+     <input type="text" onChange={ (e) => setText(e.target.value)}/> <br />
+     <button onClick={() => getMessage()}>Get message</button><br />
+     <button onClick={() => setUIMessage()}>Set message</button>
+     </center>
+    
+   </div>
   )
 }
 
